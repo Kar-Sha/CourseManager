3fc3615de5020e1e093220a9a18d46f9823065e1
GriffinDavidson <69946180+GriffinDavidson@users.noreply.github.com>
2023-04-05 18:38:36 -0700

    Updated core classes with updated Class Diagram Structure

--- src/CourseManager/Course.java
+++ src/CourseManager/Course.java
@@ -2,20 +2,43 @@ package CourseManager;
 
 public class Course 
 {
-	private int addCode;
+	private int courseID;
 	private String title;
 	private int creditHours;
 	private String description;
 	
 	public Course(
-			int addCode,
+			int courseID,
 			String title,
 			int creditHours,
 			String description)
 	{
-		this.addCode = addCode;
+		this.courseID = courseID;
 		this.title = title;
 		this.creditHours = creditHours;
 		this.description = description;
+		
+		// CourseID should be a 5 or 6 digit random number and checked to ensure any newly created
+		// courses do not match IDs with a existing course
+	}
+	
+	public int getID()
+	{
+		return this.courseID;
+	}
+	
+	public String getTitle()
+	{
+		return this.title;
+	}
+	
+	public int getCreditHours()
+	{
+		return this.creditHours;
+	}
+	
+	public String getDescription()
+	{
+		return this.description;
 	}
 }
--- src/CourseManager/CourseMap.java
+++ src/CourseManager/CourseMap.java
@@ -1,33 +1,63 @@
 package CourseManager;
 
+import java.util.ArrayList;
+
 public class CourseMap 
 {
-	private Student student;
+	private ArrayList<Course> courses;
+	private ArrayList<Course> history;
 	
-	public void generateSchedule()
+	public CourseMap()
 	{
-		
+		this.courses = new ArrayList<>();
+		this.history = new ArrayList<>();
 	}
 	
-	public void showGrades()
+	/*
+	 * Generates a formatted string of all currently active classes in courses  
+	 */
+	public String generateSchedule()
 	{
-		
+		return "";
 	}
 	
-	public void showTakenCourses()
+	/*
+	 * Generates a formatted string of all currently active class grades from courses
+	 */
+	public String showGrades()
 	{
-		
+		return "";
 	}
 	
-	// enroll *what*?
-	public void enroll()
+	/*
+	 * Shows the grades of ALL courses (history + courses) in cronological order
+	 * 	(oldest first, newest last) and shows grades for each (current classes have IN PROGRESS as grade)
+	 */
+	public String showCoursesTaken()
 	{
 		
+		return "";
 	}
 	
-	// drop *what*?
-	public void drop()
+	/*
+	 * Enrolls student into course
+	 * 
+	 * @return true if sucessfully able to add course, false otherwise
+	 */
+	public boolean enroll(int courseID)
 	{
 		
+		return false;
+	}
+	
+	/*
+	 * Drops student from currently active course, will not touch courses in history
+	 * 
+	 * @Return true if course is successfully dropped, false otherwise
+	 */
+	public boolean drop(int courseID)
+	{
+		
+		return false;
 	}
 }
