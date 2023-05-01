8ab29c03aae21befc840032ea9dffc68c4e29aed
rickynguyen12 <83847310+rickynguyen12@users.noreply.github.com>
2023-04-30 18:16:10 -0700

    Update CourseMap.java
    Added dropdown menu and functionality to switch to other screens.

--- src/CourseManager/CourseMap.java
+++ src/CourseManager/CourseMap.java
@@ -1,22 +1,28 @@
 package CourseManager;
 
 import javax.swing.*;
-
 public class CourseMap {
-    public static void main(String[] args) {
-        // Create a JFrame with title "courseMap"
-        JFrame frame = new JFrame("courseMap");
+    public JFrame frame;
+    public JComboBox<String> dropdown;
+
+    CourseMap(){
+        frame = new JFrame("Course Map");
 
         // Set the size of the frame
-        frame.setSize(500, 500);
+        frame.setSize(400, 600);
 
         // Set the default close operation
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        // Create a dropdown menu
+        String[] options = {"CourseMap", "Schedule","Course History", "Student Profile","Add/Drop Courses"};
+        dropdown = new JComboBox<String>(options);
+        dropdown.setBounds(50, 0, 300, 50);
+        frame.add(dropdown);
 
         // Make the frame visible
+        frame.setLayout(null);
         frame.setVisible(true);
     }
-
-
 }
 
