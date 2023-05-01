94aaedccee3febc0127c517020188165dee9d8f8
rickynguyen12 <83847310+rickynguyen12@users.noreply.github.com>
2023-04-30 18:23:59 -0700

    Update WelcomePage.java
    Dropdown page that switches to other frames.

--- src/CourseManager/WelcomePage.java
+++ src/CourseManager/WelcomePage.java
@@ -9,104 +9,91 @@ import javax.swing.border.Border;
 import javax.swing.BorderFactory;
 
 public class WelcomePage extends JFrame implements ActionListener {
-	private String userID;
+    public JFrame frame;
 
-	public WelcomePage(String userID, int xCoord, int yCoord) {
+    public WelcomePage() {
 
-		this.userID = userID;
+        setTitle("Welcome Back!");
+        setBounds(0, 0, 400, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		setTitle("Welcome Back " + userID);
-		setBounds(xCoord, yCoord, 400, 600);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel panel = new JPanel(null);
+        add(panel);
 
-		JPanel panel = new JPanel(null);
-		add(panel);
+        String[] options = {"Schedule", "Student Profile", "Course History", "CourseMap", "Add/Drop Courses"};
+        JComboBox<String> dropdown = new JComboBox<>(options);
+        panel.add(dropdown);
+        dropdown.setBounds(50, 0, 300, 50);
+        dropdown.addActionListener(this);
 
-		String[] options = {"Schedule", "Student Profile", "Course History", "CourseMap"};
-		JComboBox<String> dropdown = new JComboBox<>(options);
-		panel.add(dropdown);
-		dropdown.setBounds(50, 0, 300, 50);
-		dropdown.addActionListener(this);
+        JButton logoutButton = new JButton("Log Out");
+        logoutButton.addActionListener(this);
+        panel.add(logoutButton);
+        logoutButton.setBounds(50, 510, 300, 30);
 
-		JButton addCourseButton = new JButton("Add Course");
-		addCourseButton.addActionListener(this);
-		panel.add(addCourseButton);
-		addCourseButton.setBounds(50, 500, 150, 30);
+        JLabel scheduleLabel = new JLabel("Class Schedule:");
+        scheduleLabel.setBounds(30, 45, 300, 30);
+        scheduleLabel.setFont(new Font(null, Font.BOLD, 25));
+        scheduleLabel.setForeground(Color.BLUE);
+        panel.add(scheduleLabel);
 
-		JButton dropCourseButton = new JButton("Drop Course");
-		addCourseButton.addActionListener(this);
-		panel.add(dropCourseButton);
-		dropCourseButton.setBounds(200, 500, 150, 30);
+        String[] classTimes = {"9:00 AM - 10:30 AM", "10:45 AM - 12:15 PM", "1:00 PM - 2:30 PM", "2:45 PM - 4:15 PM", "4:30 PM - 6:00 PM"};
+        String[] classNames = {"Math 101", "English 101", "History 101", "Biology 101", "Computer Science 101"};
 
-		JButton logoutButton = new JButton("Log Out");
-		logoutButton.addActionListener(this);
-		panel.add(logoutButton);
-		logoutButton.setBounds(50, 540, 300, 30);
+        for (int i = 0; i < classTimes.length; i++) {
+            JPanel classPanel = new JPanel();
+            classPanel.setLayout(null);
+            Border border = BorderFactory.createLineBorder(Color.black);
+            classPanel.setBorder(border);
+            classPanel.setBounds(30, 90 + (i * 80), 340, 70);
+            panel.add(classPanel);
 
-		JLabel scheduleLabel = new JLabel("Class Schedule:");
-		scheduleLabel.setBounds(30, 45, 300, 30);
-		scheduleLabel.setFont(new Font(null, Font.BOLD, 25));
-		scheduleLabel.setForeground(Color.BLUE);
-		panel.add(scheduleLabel);
+            JLabel classTimeLabel = new JLabel(classTimes[i]);
+            classTimeLabel.setBounds(10, 10, 150, 30);
+            classPanel.add(classTimeLabel);
 
-		String[] classTimes = {"9:00 AM - 10:30 AM", "10:45 AM - 12:15 PM", "1:00 PM - 2:30 PM", "2:45 PM - 4:15 PM", "4:30 PM - 6:00 PM"};
-		String[] classNames = {"Math 101", "English 101", "History 101", "Biology 101", "Computer Science 101"};
+            JLabel classNameLabel = new JLabel(classNames[i]);
+            classNameLabel.setBounds(10, 40, 150, 30);
+            classPanel.add(classNameLabel);
+        }
 
-		for (int i = 0; i < classTimes.length; i++) {
-			JPanel classPanel = new JPanel();
-			classPanel.setLayout(null);
-			Border border = BorderFactory.createLineBorder(Color.black);
-			classPanel.setBorder(border);
-			classPanel.setBounds(30, 90 + (i * 80), 340, 70);
-			panel.add(classPanel);
+        setVisible(true);
+    }
 
-			JLabel classTimeLabel = new JLabel(classTimes[i]);
-			classTimeLabel.setBounds(10, 10, 150, 30);
-			classPanel.add(classTimeLabel);
-
-			JLabel classNameLabel = new JLabel(classNames[i]);
-			classNameLabel.setBounds(10, 40, 150, 30);
-			classPanel.add(classNameLabel);
-		}
-
-		setVisible(true);
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() instanceof JComboBox) {
-			JComboBox dropdown = (JComboBox) e.getSource();
-			String selectedOption = (String) dropdown.getSelectedItem();
-			if (selectedOption.equals("CourseMap")) {
-				JFrame courseMapFrame = new JFrame("Course Map");
-				courseMapFrame.setSize(500, 500);
-				courseMapFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-				courseMapFrame.setVisible(true);
-			}
-			if (selectedOption.equals("Student Profile")) {
-				JFrame courseMapFrame = new JFrame("Student Profile");
-				courseMapFrame.setSize(500, 500);
-				courseMapFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-				courseMapFrame.setVisible(true);
-			}
-			if (selectedOption.equals("Course History")) {
-				JFrame courseMapFrame = new JFrame("Course History");
-				courseMapFrame.setSize(500, 500);
-				courseMapFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-				courseMapFrame.setVisible(true);
-			}
-
-		} if (e.getSource() instanceof JComboBox) {
-			// code for handling the JComboBox selection
-		} else if (e.getSource() instanceof JButton) {
-			JButton button = (JButton) e.getSource();
-			if (button.getText().equals("Log Out")) {
-				dispose();
-				new LoginPage(new HashMap<>(), this.getX(), this.getY());
-			} else if (button.getText().equals("Add Course") || button.getText().equals("Drop Course")) {
-				dispose(); // dispose the WelcomePage
-				new CoursePage(); // create and show the CoursePage
-			}
-		}
-	}
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() instanceof JComboBox) {
+            JComboBox dropdown = (JComboBox) e.getSource();
+            String selectedOption = (String) dropdown.getSelectedItem();
+            if (selectedOption.equals("CourseMap")) {
+                JFrame courseMapFrame = new JFrame("Course Map");
+                courseMapFrame.setSize(400, 600);
+                courseMapFrame.setVisible(true);
+            }
+            if (selectedOption.equals("Student Profile")) {
+                JFrame studentProfileFrame = new StudentProfile().frame;
+                studentProfileFrame.setVisible(true);
+                dispose();
+            }
+            if (selectedOption.equals("Course History")) {
+                JFrame courseHistoryFrame = new CourseHistory().frame;
+                courseHistoryFrame.setVisible(true);
+                dispose();
+            }
+            if (selectedOption.equals("Add/Drop Courses")) {
+                JFrame AdjustCoursePageFrame = new AdjustCoursePage().frame;
+                AdjustCoursePageFrame.setVisible(true);
+                dispose();
+            }
+        }
+        if (e.getSource() instanceof JComboBox) {
+            // code for handling the JComboBox selection
+        } else if (e.getSource() instanceof JButton) {
+            JButton button = (JButton) e.getSource();
+            if (button.getText().equals("Log Out")) {
+                dispose();
+                new LoginPage(new HashMap<>(), this.getX(), this.getY());
+            }
+        }
+    }
 }
